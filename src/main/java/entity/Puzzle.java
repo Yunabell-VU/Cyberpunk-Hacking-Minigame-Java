58a70a7643ef4c35ae8f64608bf62a3c327600b2
Fan <yf970919@hotmail.com>
2021-02-19 13:00:23 +0100

    deleted file "Parse.java", integrated the loading process in "Puzzle.java"

--- src/main/java/entity/Puzzle.java
+++ src/main/java/entity/Puzzle.java
@@ -1,7 +1,13 @@
 package entity;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+import static java.lang.System.arraycopy;
 
 //store the pure puzzle here.
 //you can decide how to add state to each element you parsed
@@ -16,13 +22,56 @@ public class Puzzle {
     private final List<Daemon> daemons = new ArrayList<>();
     private final Buffer buffer;
 
-    public Puzzle(){
-        Parse map = new Parse();
-        map.readFile();
-        //do not modify the definitions here
-        int matrixSpan = map.getMatrixSpan();
+    private int bufferSize = 0;
+    private final String[][] matrix = new String[6][6];
+    private final List<String[]> seq = new ArrayList<>();
+    private int matrixSpan = 0;
+    private int mapSeed = 0;
+    private final Random rand = new Random();
 
-        String[][] rawMatrix = map.getMatrix();
+    private void readFile() {
+        try {
+            File dir = new File("src/main/java/txt");
+            File[] files = dir.listFiles();
+            assert files != null;
+
+            mapSeed = rand.nextInt(files.length);
+            File file = files[mapSeed];
+            mapSeed++;
+
+            Scanner reader = new Scanner(file);
+            bufferSize = reader.nextInt();
+
+            while (reader.hasNextLine()) {
+                String line = reader.nextLine();
+                if (line.length() != 0) {
+                    String[] firstLine = line.split(" ");
+                    matrixSpan = firstLine.length;
+                    arraycopy(firstLine, 0, matrix[0], 0, matrixSpan);
+                    break;
+                }
+            }
+
+            for (int i = 1; i < matrixSpan; i++)
+                for (int j = 0; j < matrixSpan; j++)
+                    matrix[i][j] = reader.next();
+
+            while (reader.hasNextLine()) {
+                String seqStr = reader.nextLine();
+                String[] seqArr = seqStr.split(" ");
+                if (seqStr.length() > 0) seq.add(seqArr);
+            }
+            //print();
+            reader.close();
+        } catch (FileNotFoundException e){
+            //out.println("Error occurs when loading puzzles");
+            e.printStackTrace();
+        }
+    }
+
+    public Puzzle(){
+        readFile();
+        //do not modify the definitions here
         MatrixCell temp;
         MatrixCell[][] emptyMatrix = new MatrixCell[matrixSpan][matrixSpan];
 
@@ -31,17 +80,16 @@ public class Puzzle {
         for (int row = 0; row < matrixSpan; row++) {
             for (int col = 0; col < matrixSpan; col++) {
                 if (row == 0) {
-                    temp = new MatrixCell(rawMatrix[row][col].toUpperCase());
+                    temp = new MatrixCell(matrix[row][col].toUpperCase());
                     temp.setAvailable(true);
                 } else {
-                    temp = new MatrixCell(rawMatrix[row][col].toUpperCase());
+                    temp = new MatrixCell(matrix[row][col].toUpperCase());
                 }
                 codeMatrix.setCell(row,col,temp);
             }
         }
 
         DaemonCell temp2;
-        List<String[]> seq = map.getSeq();
         for (String[] strings : seq) {
             ArrayList<DaemonCell> currentSeq = new ArrayList<>(strings.length);
             for (String s : strings) {
@@ -51,9 +99,33 @@ public class Puzzle {
             Daemon cookedSeq = new Daemon(currentSeq);
             daemons.add(cookedSeq);
         }
-
-        buffer = new Buffer(map.getBufferSize());
+        buffer = new Buffer(bufferSize);
     }
+
+//        public void print() {
+//        System.out.println("Current map seed: " + mapSeed);
+//        System.out.println("Current buffer size: " + bufferSize);
+//        System.out.println();
+//        System.out.println("Current matrix span: "+ matrixSpan);
+//        System.out.println("Current matrix: ");
+//        System.out.println();
+//        for (int i = 0; i < matrixSpan; i++) {
+//            for (int j = 0; j < matrixSpan; j++) {
+//                System.out.print(matrix[i][j] + " ");
+//                if (j == matrixSpan - 1) System.out.println();
+//            }
+//        }
+//        System.out.println();
+//        System.out.println("Current sequences: ");
+//        System.out.println();
+//        for (String[] strings : seq) {
+//            for (int j = 0; j < strings.length; j++) {
+//                System.out.print(strings[j] + " ");
+//                if (j == strings.length - 1) System.out.println();
+//            }
+//        }
+//    }
+
     //Do not modify the gets
     public Buffer getBuffer(){return buffer;}
 
