20667429422f14b0ca60dc46d8cc0bb3dfb1a249
Fan <yf970919@hotmail.com>
2021-03-13 16:06:18 +0100

    ScoreHandler implemented

--- src/main/java/entity/ScoreHandler.java
+++ src/main/java/entity/ScoreHandler.java
@@ -1,13 +1,48 @@
 package entity;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Scanner;
 
 public class ScoreHandler {
 
     private ScoreHandler() { throw new IllegalStateException("Utility class"); }
 
     public static int importHighestScore(){
-        return 100; //TODO
+        int highestScore = 0;
+        try {
+            File saveFile = new File("./resources/save/score");
+            Scanner reader = new Scanner(saveFile);
+            while (reader.hasNext()) {
+                String rawScore = reader.nextLine();
+                highestScore = parseScore(rawScore);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Error occurs when loading score");
+            e.printStackTrace();
+        }
+        return highestScore;
     }
+
     public static void exportHighestScore(int score){
-        System.out.println("Score " + score + " saved");//TODO
+        try {
+            FileWriter scoreWriter = new FileWriter("./resources/save/score");
+            String cookedScore = encryptScore(score);
+            scoreWriter.write(cookedScore);
+            scoreWriter.close();
+        } catch (IOException e) {
+            System.out.println("Error occurs when saving score");
+            e.printStackTrace();
+        }
+    }
+
+    private static String encryptScore(int score) {
+        return Base64.getEncoder().encodeToString(String.valueOf(score).getBytes(StandardCharsets.UTF_8));
+    }
+
+    private static int parseScore(String score) {
+        byte[] scoreBytes = Base64.getDecoder().decode(score);
+        String scoreString = new String(scoreBytes, StandardCharsets.UTF_8);
+        return Integer.parseInt(scoreString);
     }
 }
